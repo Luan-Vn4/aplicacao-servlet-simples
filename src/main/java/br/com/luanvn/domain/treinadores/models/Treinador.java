d27f7f8012fa803f8086b07d69f7565b70713db5
Luan-Vn4 <luanvilaca1@gmail.com>
2024-10-24 00:44:34 -0300

    feat: criada entidade de treinador

---
+++ src/main/java/br/com/luanvn/domain/treinadores/models/Treinador.java
@@ -0,0 +1,55 @@
+package br.com.luanvn.domain.treinadores.models;
+
+public class Treinador {
+
+    private int id;
+
+    private String apelido;
+
+    private String email;
+
+    private String senha;
+
+    private boolean isProfessor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isProfessor() {
+        return isProfessor;
+    }
+
+    public void setProfessor(boolean professor) {
+        isProfessor = professor;
+    }
+
+}
